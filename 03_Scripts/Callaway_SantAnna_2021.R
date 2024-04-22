5ef1189a752f816e508e0a7cb1dcc825c0367557
alexander-j-vogt <alexander@vogt-space.de>
2024-04-22 21:25:45 +0200

    ID Variables

--- 03_Scripts/Callaway_SantAnna_2021.R
+++ 03_Scripts/Callaway_SantAnna_2021.R
@@ -1,4 +1,4 @@
-rm(list = ls())
+rm(list = ls(all = TRUE))
 
 library(tidyverse)
 library(haven)
@@ -6,9 +6,6 @@ library(haven)
 
 data_path <- "01_Data"
 table_path <- "02_Tables"
-
-
-# Import the data of QWI as API
 
 # Import dataset of QWI for the period 1995 to 2010
 qwi_po_raw <- read_csv(paste0("./", data_path, "/", "qwi_all_privateownership.csv"))
@@ -20,7 +17,7 @@ View(qwi_raw)
 
 # Data Cleaning - Firms with all private ownership
 
-qwi_pop_raw <- qwi_po_raw |> 
+qwi_po <- qwi_po_raw |> 
   select(geography, geography_label.value, agegrp, agegrp_label.value, 
          year, quarter, Emp, EmpEnd, EmpS, EmpSpv, EmpTotal, sEmp, sEmpEnd,
          sEmpS, sEmpSpv, sEmpTotal) |> 
@@ -28,15 +25,22 @@ qwi_pop_raw <- qwi_po_raw |>
          agegrp_label = agegrp_label.value) |>
   filter(agegrp == "A01")
 
+# Create Key Variables for clear identificiation: county, state and year
+qwi_po <- qwi_po |> 
+  separate(col = geography_label, into = c("county", "state"), sep = ",", remove = TRUE, fill = "right", extra = "drop") |>
+  mutate(state = str_trim(state, side = "both"),
+         county = str_replace_all(county, " ", "_"),
+         date_q = paste0(year, ": Q", quarter)) |>
+  mutate(date_q = yq(date_q)) |>
+  relocate(date_q, .after = "state") |>
+  select(-c("geography", "agegrp", "agegrp_label"))
 
-qwi_pop_raw |>
-  str_split(geography_label, ",", simplify = TRUE) |>
-  as_tibble() |>
-  mutate(
-    county = .[[1,1]],
-    state  = .[[1,2]] 
-  ) |> View()
-  
-  
-unique(test["geography"])
-  View()
+
+
+
+
+
+
+################################################################################
+################################# END ##########################################
+################################################################################
