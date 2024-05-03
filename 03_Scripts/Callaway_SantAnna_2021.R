5fdd720fb1505f5d65379db5d97a2e175383c25b
alexander-j-vogt <alexander@vogt-space.de>
2024-05-03 16:09:54 +0200

    WIP

--- 03_Scripts/Callaway_SantAnna_2021.R
+++ 03_Scripts/Callaway_SantAnna_2021.R
@@ -7,9 +7,11 @@ library(haven)
 data_path <- "01_Data"
 table_path <- "02_Tables"
 
-# Import dataset of QWI for the period 1995 to 2010
+# Import data set of QWI for the period 1995 to 2010
 qwi_po_raw <- read_csv(paste0("./", data_path, "/", "qwi_all_privatownership.csv"))
 
+# Import the cleaned "2000 County Data Book" dataset
+cdb_2000 <- read_rds(paste0("./", data_path, "/", "county_data_book_2000.rds"))
 
 # Identification of Region and State accroding to US Bureau of Labor Statistics
 region_abbreviations <- c(
@@ -30,7 +32,37 @@ region_abbreviations <- c(
   WI = "Midwest", WY = "West"
 )
 
-# Restrict the dataset according to the procdure of Cattaway & Sant'Anna (2021)
+
+treated_general <- c("AZ", "AK", "CO", "FL", "IL", "ME", "MD", "MI", "MN", "MO",
+                     "MT", "NV", "NM", "NC", "OH", "PA", "WV", "I")
+
+above_fed_mw <- c("AK", "CT", "HI", "MA", "OR", "VT")
+
+# Select the relevant pre-treatment county characteristics
+
+cdb_2000_restricted <- cdb_2000 |> 
+  select(county,
+         state,
+         state_abbrev,
+         pop_nr_2000, # Total Population of each county in 2000
+         nr_white_2000, # Total White Population for each county in year 2000
+         Median_Inc_1997_USD, # Median Income (USD) for each county in 1997 
+         Educ_Attain1990_HS_Perc, # Fraction of High-School graduates or a higher degree in 1990 
+         Pov97_Perc_AllAge # Fraction of persons of all ages below poverty level in 1997
+         ) |>
+  mutate(white_pop_2000_perc = nr_white_2000 / pop_nr_2000,
+         pop_2000_nr_1000s = pop_nr_2000 / 1000,
+         median_income_1997_1000s = Median_Inc_1997_USD / 1000) |>
+  rename(poverty_allages_1997_perc = Pov97_Perc_AllAge,
+         HS_1990_perc = Educ_Attain1990_HS_Perc,
+         state_name = state,
+         state = state_abbrev) |>
+  relocate(state, .after = county) |>
+  arrange(state, county)
+
+
+
+# Restrict the data set according to the procedure of Callaway & Sant'Anna (2021)
 # Described in the Supplementary Appendix
 
 
@@ -42,7 +74,7 @@ qwi_po <- qwi_po_raw |>
          sEmpS, sEmpSpv, sEmpTotal) |> 
   rename(geography_label = geography_label.value,
          agegrp_label = agegrp_label.value) |>
-  filter(agegrp == "A01")
+  filter(agegrp == "A01") 
 
 # Create Key Variables for clear identification: county, state and year
 qwi_po <- qwi_po |> 
@@ -51,16 +83,12 @@ qwi_po <- qwi_po |>
          county = str_replace_all(county, " ", "_"),
          date_q = paste0(year, ": Q", quarter)) |>
   mutate(date_q = yq(date_q)) |>
+  mutate(date_q = as.Date(date_q)) |>
   relocate(date_q, .after = "state") |>
-  select(-c("geography", "agegrp", "agegrp_label"))
-
-# Filter for all relevant states and create a region indicator 
-qwi_po <- qwi_po |> 
-  filter(!state %in% c("AL", "AK", "AZ", "AR", "CA", "CT", "DE", "HI", "KY", 
-                       "ME", "MA", "MS", "NH", "NJ", "NY", "OR", "PA", "RI",
-                       "VT", "WA", "WY", "DC", "PR")) |>
-  mutate(region = region_abbreviations[state]) |>
-  relocate(region, .after = state)
+  select(-c("geography", "agegrp", "agegrp_label")) |>
+  filter(!state %in% c("DC", "PR")) |>
+  arrange(state, county) |>
+  mutate(county = str_replace_all(county, "-", "_"))
 
 # Create Year-Indicator of Treatment
 qwi_po <- qwi_po |>
@@ -69,12 +97,52 @@ qwi_po <- qwi_po |>
          treat_g2007 = ifelse(state %in% c("CO", "MD", "MI", "MO", "MT", "NV" ,
                                            "NC", "OH", "WV" ), 1, 0))
 
+# Filter for all relevant states and create a region indicator 
+qwi_restricted <- qwi_po |> 
+  filter(!state %in% c("AL", "AK", "AZ", "AR", "CA", "CT", "DE", "HI", "KY", 
+                       "ME", "MA", "MS", "NH", "NJ", "NY", "OR", "PA", "RI",
+                       "VT", "WA", "WY", "DC", "PR")) |>
+  mutate(region = region_abbreviations[state]) |>
+  relocate(region, .after = state)
+
+# qwi_matched 
+# qwi_po_matched <- 
 qwi_po |>
-  filter(sEmp == 5) |>
-  distinct(Emp)
+  full_join(cdb_2000_restricted, by = c("state", "county")) |> #View()
+  filter(date_q == "2001-01-01") |>
+  distinct(county, state)
 
+return(qwi_po_matched)
 
+qwi_po |>
+  filter(!state %in% above_fed_mw) |>
+  filter(!sEmp == 5) |>
+  distinct(county, state)
 
+test <- qwi_po |> 
+  filter((date_q >= "2003-04-01") & (date_q <= "2007-01-01")) |>
+  mutate(treat_general = ifelse(state %in% above_fed_mw, 1, 0))
+
+qwi_po |>
+  filter(year == 2001) |>
+  distinct(county)
+
+test <-  qwi_po |> filter(str_detect(date_q, regex("-01-01"))) 
+
+v <- data.frame(year = rep(NA, 8), county_nr = rep(NA, 8))
+
+for(i in 2001:2007) {
+
+  v[i - 2000, 1] <- i
+    
+  v[i - 2000, 2] <- qwi_po |> 
+  filter(year == i) |>
+  distinct(county, state) |>
+  nrow()
+}
+  
+
+v
 
 
 
