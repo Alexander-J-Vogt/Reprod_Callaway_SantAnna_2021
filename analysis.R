66286b88023643fb490d337df2b999cfe68575ae
alexander-j-vogt <alexander@vogt-space.de>
2024-05-12 18:27:25 +0200

    attgt-WIP-loop; #6

--- analysis.R
+++ analysis.R
@@ -45,18 +45,17 @@ qwi <- qwi |>
 # covariates formula
 spec_formula <- ~-1+white_pop_2000_perc+poverty_allages_1997_perc+pop_2000_nr_1000s+median_income_1997_1000s+HS_1990_perc
 
-# copy of qwi
-data <- qwi
 
 # relevant numbers
 timelist <- unique(qwi$date_y)
-grouplist <- unique(qwi$group)
+grouplist <- sort(unique(qwi$group))
+grouplist <- grouplist[grouplist != 0]
 num_of_cases <- length(timelist) * length(grouplist)
 
 # create empty data frame and/or list
 attgt.df <- as.data.frame(matrix(NA, nrow = num_of_cases, ncol = 3))
 colnames(attgt.df) <- c("attgt", "group", "year")
-att.gt.ls <- ls()
+att.gt.ls <- list()
 
 # create empty matrix
 number <- 1
@@ -68,81 +67,112 @@ nr_times <- length(timelist)
 if_matrix <- Matrix::Matrix(data = 0, nrow = n_unique, 
                             ncol = nr_group * (nr_times - 1), 
                             sparse = TRUE)
-nrow(if_matrix)
+
 
 
 # ---- Calculation and later loop ----------------------------------------------
 
-# 
-# if(period >= group) {
-#   reference_year <- group - 1
-# } else {
-#   reference_year <- period - 1 
-# }
+# copy of qwi
+data <- qwi
 
+for (g in grouplist) {
+  #g <- 2004
+  for (t in timelist) {
+  
+    data <- qwi
+    
+    print(paste0("iteration: ", number))
+    
+    print(paste0("group: ", g, " & period ", t))
+    
+    # determine reference_year based on whether t lays within the post-treatment 
+    # period or not
+    if(t >= g) {
+      reference_year <- g - 1
+    } else {
+      reference_year <- t
+    }
+    
+    
+    print(paste0("reference year: ", reference_year))
+    
+    # current group indicator (should get overwritten once we loop over groups)
+    data$g_ <- ifelse(data$group == g, 1, 0) # loop
+    data$c_ <- ifelse(data$group == 0   , 1, 0)
+    
+    # Select data in pre- & post-treatment period for relevant group and never-treated
+    data <- subset(data, date_y %in% c(reference_year, t + 1)) # must be replaced by reference year and current period in loop
+    
+    View(data)
+    
+    # indicator for influence matrix
+    index_data    <- data[!duplicated(data$county_id),] # loop 
+    index_inffunc <- (index_data$g_ == 1) | (index_data$c_ == 1)
+    n_sample      <- length(unique(index_data$county_id))
+    
+    # select group & nevertreated (control)
+    index_gc <- (data$g_ == 1) | (data$c_ == 1)
+    data_sel <- data[index_gc,]
+    n_subset <- length(unique(data_sel$county_id))
+    
+    # Create treatment indicator and date variables as character
+    data_sel <- data_sel|> 
+      mutate(date = paste0("y", date_y), treat = ifelse(group == g, 1, 0))
+    
+    View(data_sel)
+    
+    # Use BMisc::panel2cs2 to transform the two period panel dataset into a
+    # into a cross-sectional data set, which is required for the function, which
+    # calculates the Doubly Robust DiD-estimator
+    data_cs <- BMisc::panel2cs2(data_sel, 
+                                yname  = "lnEmp", 
+                                idname = "county_id", 
+                                tname  = "date", 
+                                balance_panel = FALSE)
+    
+    # If the number of rows is odd, the BMisc::panel2cs2 function produces missing 
+    # in either .y1 or .y0. Thus, the NA's need to be removed, otherwise we don't
+    # yield any DiD-estimator
+    data_cs <- data_cs[!is.na(.y1),]
+    data_cs <- data_cs[!is.na(.y0),]
+    
+    # Save a matrix of covariates
+    covariates <- model.matrix(spec_formula, data_cs)
+    
+    # att of group g at time point t
+    att <- DRDID::drdid_panel(y1 = data_cs$.y1, 
+                              y0 = data_cs$.y0,
+                              D  = data_cs$treat,
+                              covariates = covariates,
+                              inffunc    = TRUE,
+                              boot       = FALSE)
+    
+    # recover att
+    att.gt.ls[[number]]<- list(attgt = att$ATT, group = g, period = t + 1)
+    
+    # Data Frame of ATTgt
+    # attgt.df[1, 1] <- att$ATT 
+    # attgt.df[1, 2] <- 2004
+    # attgt.df[1, 3] <- 2003
+    
+    
+    ## Recover influence function
+    # Create a empty vector with the length of unique observations
+    if_vector <- rep(0, n_sample)
+    
+    # estimate of influence function, weighted by relative sample size &
+    if_vector[index_inffunc] <- (n_subset / n_sample) * att$att.inf.func
+    
+    # save vector of round x into the column
+    if_matrix[, number] <- if_vector
 
+    # add 1 to number for index
+    number <- number + 1
+    
+    
+  }
 
-# current group indicator (should get overwritten once we loop over groups)
-data$g <- ifelse(data$group == 2004, 1, 0)
-data$c <- ifelse(data$group == 0   , 1, 0)
-
-# Select data in pre- & post-treatment period for relevant group and never-treated
-data <- subset(data, date_y %in% c(2003, 2004)) # must be replaced by reference year and current period in loop
-
-
-
-# indicator for influence matrix
-index_data <- data[data$date_y == 2004,]
-index_inffunc <- (index_data$g == 1) | (index_data$c == 1)
-n_sample <- length(unique(index_data$county_id))
-
-# select group & nevertreated (control)
-index_gc <- (data$g == 1) | (data$c == 1)
-data_sel <- data[index_gc,]
-n_subset <- length(unique(data_sel$county_id))
-
-# Create treatment indicator and date variables as character
-data_sel <- data_sel|> 
-  mutate(date = paste0("y", date_y), treat = ifelse(group == 2004, 1, 0))
-
-# Use BMisc::panel2cs2 to transform the two period panel dataset into a
-# into a cross-sectional data set, which is required for the function, which
-# calculates the Doubly Robust DiD-estimator
-data_cs <- BMisc::panel2cs2(data_sel, yname = "lnEmp", idname = "county_id", 
-                            tname = "date", balance_panel = FALSE)
-
-# If the number of rows is odd, the BMisc::panel2cs2 function produces missing 
-# in either .y1 or .y0. Thus, the NA's need to be removed, otherwise we don't
-# yield any DiD-estimator
-data_cs <- data_cs[!is.na(.y1),]
-data_cs <- data_cs[!is.na(.y0),]
-
-# Save a matrix of covariates
-covariates <- model.matrix(spec_formula, data_cs)
-
-# att of group g at time point t
-att <- DRDID::drdid_panel(y1 = data_cs$.y1, 
-                          y0 = data_cs$.y0,
-                          D  = data_cs$treat,
-                          covariates = covariates,
-                          inffunc    = TRUE,
-                          boot       = FALSE)
-
-# recover att
-att.gt.ls[[1]]<- list(attgt = att$ATT, group = 2004, period = 2003)
-
-# Data Frame of ATTgt
-# attgt.df[1, 1] <- att$ATT 
-# attgt.df[1, 2] <- 2004
-# attgt.df[1, 3] <- 2003
-
-# recover influence function
-if_vector <- rep(0, n_sample)
-# estimate of influence function, weighted by relative sample size
-if_vector[index_inffunc] <- (n_subset / n_sample) * att$att.inf.func
-# save vector of round x into the column
-if_matrix[,1] <- if_vector
-
+}
 
 
 
